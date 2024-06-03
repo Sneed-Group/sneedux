f38b912dd130d9151c07a50ece62c44b413df6b2
Sam Sneed <163201376+sam-sneed@users.noreply.github.com>
2024-06-03 21:27:45 +0000

    Update maker.sh

--- maker.sh
+++ maker.sh
@@ -1,3 +1,4 @@
+sudo apt install wget build-essential bison flex libncurses-dev libssl-dev libelf-dev
 make -j20 && sudo make modules_install -j20
 sudo cp arch/x86/boot/bzImage /boot/vmlinuz-sneedux
 sudo grub-mkconfig -o /boot/grub/grub.cfg
